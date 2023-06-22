b58c348a78456e31739fb75cbf9cd1ac4a92c93b
sureshbadikela <90884313+sureshbadikela@users.noreply.github.com>
2023-06-22 09:41:02 +0530

    flutter project

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Text('hello world',
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.blue)));
+}
